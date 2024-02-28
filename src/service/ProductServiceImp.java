e9ee6d9b3c62e405623404d9a29bda99da8e149b
chansamangrathana <chansamangrathana1607@gmail.com>
2024-02-28 10:49:14 -0800

    implement remove product

--- src/service/ProductServiceImp.java
+++ src/service/ProductServiceImp.java
@@ -2,6 +2,7 @@ package service;
 
 import model.Product;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -23,10 +24,41 @@ public class ProductServiceImp implements ProductService {
 
     }
 
-    @Override
     public void removeProduct(List<Product> products, Scanner input, String fileName) {
+        System.out.println("Input ID that you want to remove: ");
+        input.nextLine();
+        String id = input.nextLine();
+        boolean removed = false;
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            StringBuilder updatedContent = new StringBuilder();
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts[0].equals(id)) {
+                    removed = true;
+                    System.out.println("Product with ID: " + id + " removed Successfully!");
+                    continue;
+                }
+                updatedContent.append(line).append(System.lineSeparator());
+            }
 
+            if (!removed) {
+                System.out.println("Product with ID: " + id + " not found.");
+                return;
+            }
+
+            // Write updated content back to the file
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+                writer.write(updatedContent.toString());
+            } catch (IOException e) {
+                System.err.println("Error writing to file: " + e.getMessage());
+            }
+
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        }
     }
+
 
     @Override
     public void searchByName(Scanner input, List<Product> products, String fileName) {
